f80d545d7b81d4ba02dfdadac08b246d142627c1
Ryan P. Wilson <rpw@authorityfx.com>
2020-04-09 22:24:33 -0400

    Add .cmake-format.yaml and format cmake files

--- cmake/force_colored_output.cmake
+++ cmake/force_colored_output.cmake
@@ -1,10 +1,13 @@
+# Force colored output for prettier Ninja console logging
 function(force_colored_output)
-  option(FORCE_COLORED_OUTPUT "Always produce ANSI-colored output (GNU/Clang only)." TRUE)
-  if (${FORCE_COLORED_OUTPUT})
-    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-      add_compile_options (-fdiagnostics-color=always)
-    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-      add_compile_options (-fcolor-diagnostics)
-    endif ()
-  endif ()
+  option(FORCE_COLORED_OUTPUT
+         "Always produce ANSI-colored output (GNU/Clang only)." TRUE
+  )
+  if(${FORCE_COLORED_OUTPUT})
+    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+      add_compile_options(-fdiagnostics-color=always)
+    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+      add_compile_options(-fcolor-diagnostics)
+    endif()
+  endif()
 endfunction()
--- cmake/install_cmake_format.cmake
+++ cmake/install_cmake_format.cmake
@@ -1,42 +1,34 @@
+# Install cmake-format using pip
 function(install_cmake_format)
   # Just return is cmake is already marked in cache as installed
-  if (_cmake_format_installed EQUAL true)
+  if(CMAKE_FORMAT_INSTALLED EQUAL true)
     return()
   endif()
   # check list of installed pip packages
   execute_process(
-    COMMAND
-      pip list
-    OUTPUT_VARIABLE
-      _output
-    ERROR_VARIABLE
-      _error
-    RESULT_VARIABLE
-      _return
+    COMMAND pip list
+    OUTPUT_VARIABLE _output
+    ERROR_VARIABLE _error
+    RESULT_VARIABLE _return
   )
   # Throw is return code not 0
-  if (NOT _return EQUAL 0)
+  if(NOT _return EQUAL 0)
     message(FATAL_ERROR ${_error})
   endif()
   # Regex for cmake-format in package list
-  string(
-    REGEX MATCH
-    "cmake-format"
-    _match
-    ${_output}
-  )
+  string(REGEX MATCH "cmake-format" _match ${_output})
   # If cmake-list not found in package list, install for user (avoid sudo)
-  if (NOT _match)
+  if(NOT _match)
     message(STATUS "Installing cmake-format")
     execute_process(
-      COMMAND
-        pip install --user cmake-format
-      RESULT_VARIABLE
-        _return
+      COMMAND pip install --user cmake-format RESULT_VARIABLE _return
     )
   endif()
   # Set cache variable to true if _return code 0
-  if (_return EQUAL 0)
-    set(_cmake_format_installed true CACHE BOOL "cmake-format is installed")
+  if(_return EQUAL 0)
+    set(CMAKE_FORMAT_INSTALLED
+        true
+        CACHE BOOL "cmake-format is installed"
+    )
   endif()
 endfunction()
